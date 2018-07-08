392011e08f1cc2946b9607c9709f5bc8974d7866
Shawn Baugh II <shawnbaughcreative@gmail.com>
2018-07-07 22:20:53 -0500

    Styled and edited to plate to fit my vision. Added in photos and links to social media. Added new icon.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,73 @@
 import React from 'react'
 
-const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
-        <div className="logo">
-            <span className="icon fa-diamond"></span>
-        </div>
-        <div className="content">
-            <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
-            </div>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+const logoPic = require('../images/logo.png')
+
+const Header = props => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      {/* <span className="icon fa-fire" /> */}
+      <img className="" src={logoPic} alt="Baugh Creative Logo" />
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>Shawn Baugh 2</h1>
+        <p>
+          Just a creative that loves, makes, and performs music.
+          <br />
+          Follow the journey.
+        </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('intro')
+            }}
+          >
+            Music
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('work')
+            }}
+          >
+            Videos
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            My Journey
+          </a>
+        </li>
+        <li>
+          <a
+            href="javascript:;"
+            onClick={() => {
+              props.onOpenArticle('contact')
+            }}
+          >
+            Subscribe
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </header>
 )
 
 Header.propTypes = {
-    onOpenArticle: React.PropTypes.func,
-    timeout: React.PropTypes.bool
+  onOpenArticle: React.PropTypes.func,
+  timeout: React.PropTypes.bool,
 }
 
 export default Header
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,42 +1,133 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-import pic01 from '../images/pic01.jpg'
+import heyyoupic from '../images/heyyoucard.png'
+import ffwmpic from '../images/ffwmcard.png'
+import honestycard from '../images/honestycard.png'
+import damncard from '../images/damncard.png'
+import mindgamescard from '../images/mindgamescard.png'
 import pic02 from '../images/pic02.jpg'
-import pic03 from '../images/pic03.jpg'
+import pic03 from '../images/aboutpic.png'
 
 class Main extends React.Component {
   render() {
-
-    let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
+    let close = (
+      <div
+        className="close"
+        onClick={() => {
+          this.props.onCloseArticle()
+        }}
+      />
+    )
 
     return (
-      <div id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
-
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Intro</h2>
-          <span className="image main"><img src={pic01} alt="" /></span>
-          <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+      <div
+        id="main"
+        style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
+      >
+        <article
+          id="intro"
+          className={`${this.props.article === 'intro' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Music</h2>
+          <span className="image main">
+            <img src={heyyoupic} alt="" />
+          </span>
+          <p>Hey You - Out Now on your favorite streaming services.</p>
+          <span className="image main">
+            <img src={ffwmpic} alt="" />
+          </span>
+          <p>
+            First For Whats Mine - Out Now on your favorite streaming services.
+          </p>
+          <span className="image main">
+            <img src={honestycard} alt="" />
+          </span>
+          <p>Honesty - Out Now on your favorite streaming services.</p>
+          <span className="image main">
+            <img src={damncard} alt="" />
+          </span>
+          <p>Damn - Out Now on your favorite streaming services.</p>
+          <span className="image main">
+            <img src={mindgamescard} alt="" />
+          </span>
+          <p>Mind Games - Out Now on your favorite streaming services.</p>
           {close}
         </article>
 
-        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Work</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+        <article
+          id="work"
+          className={`${this.props.article === 'work' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
+          <h2 className="major">Videos</h2>
+          <span className="image main">
+            <iframe
+              width="560"
+              height="215"
+              src="https://www.youtube.com/embed/KnpA2Ggvh0U"
+              frameborder="0"
+              allow="autoplay; encrypted-media"
+              allowfullscreen
+            />
+          </span>
+          <p>Check out how I created my song Hey You.</p>
+          <span className="image main">
+            <iframe
+              width="560"
+              height="215"
+              src="https://www.youtube.com/embed/_sDkmb2Ftz0"
+              frameborder="0"
+              allow="autoplay; encrypted-media"
+              allowfullscreen
+            />
+          </span>
+          <p>Check out how I created this unreleased song.</p>
+          <span className="image main">
+            <iframe
+              width="560"
+              height="215"
+              src="https://www.youtube.com/embed/VmglFi-6u_M"
+              frameborder="0"
+              allow="autoplay; encrypted-media"
+              allowfullscreen
+            />
+          </span>
+          <p>Check out how I created my song Mind Games.</p>
           {close}
         </article>
 
-        <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article
+          id="about"
+          className={`${this.props.article === 'about' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
           <h2 className="major">About</h2>
-          <span className="image main"><img src={pic03} alt="" /></span>
-          <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
+          <span className="image main">
+            <img src={pic03} alt="" />
+          </span>
+          <p>
+            Born and raised from Fresno, CA, I've always been intrigued by music
+            and performing. I grew up in a very musical home and was always
+            either playing music or playing tennis.
+          </p>
           {close}
         </article>
 
-        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
+        <article
+          id="contact"
+          className={`${this.props.article === 'contact' ? 'active' : ''} ${
+            this.props.articleTimeout ? 'timeout' : ''
+          }`}
+          style={{ display: 'none' }}
+        >
           <h2 className="major">Contact</h2>
           <form method="post" action="#">
             <div className="field half first">
@@ -49,22 +140,45 @@ class Main extends React.Component {
             </div>
             <div className="field">
               <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
+              <textarea name="message" id="message" rows="4" />
             </div>
             <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
+              <li>
+                <input type="submit" value="Send Message" className="special" />
+              </li>
+              <li>
+                <input type="reset" value="Reset" />
+              </li>
             </ul>
           </form>
           <ul className="icons">
-            <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="#" className="icon fa-facebook"><span className="label">Facebook</span></a></li>
-            <li><a href="#" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
-            <li><a href="#" className="icon fa-github"><span className="label">GitHub</span></a></li>
+            <li>
+              <a
+                href="https://twitter.com/baughcreative"
+                className="icon fa-twitter"
+              >
+                <span className="label">Twitter</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://facebook.com/baughcreative"
+                className="icon fa-facebook"
+              >
+                <span className="label">Facebook</span>
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://instagram.com/baughcreative"
+                className="icon fa-instagram"
+              >
+                <span className="label">Instagram</span>
+              </a>
+            </li>
           </ul>
           {close}
         </article>
-
       </div>
     )
   }
@@ -75,7 +189,7 @@ Main.propTypes = {
   article: React.PropTypes.string,
   articleTimeout: React.PropTypes.bool,
   onCloseArticle: React.PropTypes.func,
-  timeout: React.PropTypes.bool
+  timeout: React.PropTypes.bool,
 }
 
 export default Main
